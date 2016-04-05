66c2b054c5768dda7e5575af51882b29e5b6b2d1
Navin Peiris <navinpeiris@gmail.com>
2016-04-05 15:36:43 +0530

    Update version to 0.1.1

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule ExUnitNotifier.Mixfile do
 
   def project do
     [app: :ex_unit_notifier,
-     version: "0.1.0",
+     version: "0.1.1",
      name: "ExUnitNotifier",
      description: "Show status notifications for ExUnit test runs",
      source_url: "https://github.com/navinpeiris/ex_unit_notifier",
