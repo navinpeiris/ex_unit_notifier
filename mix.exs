f9e174b8a4eec8e6fd7f5329348dbca77d009426
Navin Peiris <navinpeiris@gmail.com>
2020-12-18 17:17:33 +0530

    Bump version to 1.0.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule ExUnitNotifier.Mixfile do
   def project do
     [
       app: :ex_unit_notifier,
-      version: "0.1.4",
+      version: "1.0.0",
       name: "ExUnitNotifier",
       description: "Show status notifications for ExUnit test runs",
       source_url: "https://github.com/navinpeiris/ex_unit_notifier",
