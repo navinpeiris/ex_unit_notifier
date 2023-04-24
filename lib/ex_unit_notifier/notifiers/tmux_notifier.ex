d70fdf3d01d3c533e14d7723ae9b3b2f127cec05
Navin Peiris <navinpeiris@gmail.com>
2023-04-24 12:38:00 +0530

    Check for tmux availability

--- lib/ex_unit_notifier/notifiers/tmux_notifier.ex
+++ lib/ex_unit_notifier/notifiers/tmux_notifier.ex
@@ -2,15 +2,22 @@ defmodule ExUnitNotifier.Notifiers.TmuxNotifier do
   @moduledoc false
 
   def notify(status, _message, _opts) do
-    pane = System.get_env("TMUX_PANE")
-
-    if pane do
+    if pane() do
       # tmux set-window-option -t"$TMUX_PANE" window-status-style bg=red
-      System.cmd("tmux", ["set-window-option", "-t#{pane}", "window-status-style", style(status)])
+      System.cmd(executable(), [
+        "set-window-option",
+        "-t#{pane()}",
+        "window-status-style",
+        style(status)
+      ])
     end
   end
 
-  def available?, do: true
+  def available?, do: executable() != nil && pane() != nil
+
+  defp executable, do: System.find_executable("tmux")
+
+  defp pane, do: System.get_env("TMUX_PANE")
 
   defp style(status) do
     if status == :error do
