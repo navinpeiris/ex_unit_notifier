d105e5051ba2901198901590c3e11f5c58a58b9b
c4lliope <code@assembled.app>
2023-11-16 14:27:35 -0500

    Pull notification logic; use generalized `ding`.

--- lib/ex_unit_notifier/notifiers/terminal_title.ex
+++
@@ -1,7 +0,0 @@
-defmodule ExUnitNotifier.Notifiers.TerminalTitle do
-  @moduledoc false
-
-  def notify(_status, message, _opts), do: IO.puts(:stderr, "\e]2;ExUnit - #{message} \a")
-
-  def available?, do: true
-end
